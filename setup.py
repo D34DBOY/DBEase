0ffaeefbbaebd955c8becc97f1e7fbd13c90844a
DeadBoy <61543491+D34DBOY@users.noreply.github.com>
2024-08-12 16:10:31 +0330

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="DBEase",
+    version="0.1.0", 
+    author="Mohammad Mohammadi Bijaneh", 
+    author_email="hiostad6@gmail.com", 
+    description="This is a program designed to facilitate communication with the database, preventing unnecessary congestion in the source.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/D34DBOY/DBEase",
+    packages=find_packages(),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10"
+    ],
+    python_requires='>=3.8',
+    install_requires=[
+
+    ],
+)
